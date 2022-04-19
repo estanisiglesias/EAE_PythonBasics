7acc5ead5494af39a370909accc6ba7664250583
Estanis Iglesias <estanis.iglesias@uvesolutions.com>
2022-04-19 20:01:36 +0200

    Ejercicios hechos en clase

--- ejercicios.py
+++ ejercicios.py
@@ -1,1 +1,66 @@
+# %%
+#declaració e inicialización de la variable var1
+var1='Hola'
+
+# %%
+print(var1)
+
+# %%
+7+3
+
+# %%
+ahorro=100
+rend=1.1
+anyos=7
+
+# %%
+importeFinal=ahorro*rend**7
+
+# %%
+altura=1.79
+peso=68.7
+imc=0
+
+# %%
+imc=peso/altura**2
+
+# %%
+print(imc)
+
+# %%
+type(var1)
+
+# %%
+'Hello' + ' ' + 'world!!'
+
+# %%
+2+3
+
+# %%
+ahorro=100
+print(ahorro)
+
+# %%
+desc='interés compuesto'
+rentable=True
+
+# %%
+doubledesc=desc+desc
+
+# %%
+print(doubledesc)
+
+# %%
+print('Con un ahorro incial de ' + str(ahorro) + ' he obtenido un beneficio de: ' + str(importeFinal) + ' pasados ' + str(anyos) + ' años')
+
+# %%
+pi_string='3.1415'
+
+# %%
+pi_float=float(pi_string)
+
+# %%
+10 + pi_float
+
+
 
